6f25552bf8e02c1e94455ede5e75402e2480df24
Ahn ye ji <ayj98@naver.com>
2025-04-08 18:02:16 +0900

    Feat: DTO 추가

---
+++ src/com/team1/mybatis/IAgesPreferedDAO.java
@@ -0,0 +1,12 @@
+/*==============================
+	IAgesPreferedDAO.java
+	- 선호 연령대 IDAO
+
+=================================*/
+
+package com.team1.mybatis;
+
+public interface IAgesPreferedDAO
+{
+
+}
